31b925212dbd3f541f8b7790aa3fe034cce3b6f1
Enrico <epilotto@gmx.com>
2019-05-10 00:42:01 +0200

    Add prettier

--- eslintrc.js
+++ eslintrc.js
@@ -1,23 +1,30 @@
 module.exports = {
-  "extends": "standard",
-  "globals": {
-    "self": false,
-    "$": false,
-    "Turbolinks": false
+  extends: 'standard',
+  globals: {
+    self: false,
+    $: false,
+    I18n: false,
+    Turbolinks: false,
+    history: false
   },
-  "plugins": ["react"],
-  "parser": "babel-eslint",
-  "rules": {
-    "curly": ["error", "multi-or-nest", "consistent"],
-    "key-spacing": ["error", { "mode": "minimum" }],
-    "no-eval": "off",
-    "no-multi-spaces": ["error", { exceptions: {
-      "VariableDeclarator": true,
-      "ImportDeclaration": true
-    } }],
-    "no-return-assign": "off",
-    "space-before-function-paren": ["error", "never"],
-    "react/jsx-uses-react": "error",
-    "react/jsx-uses-vars": "error"
+  plugins: ['react', 'prettier'],
+  parser: 'babel-eslint',
+  rules: {
+    'key-spacing': ['error', { mode: 'minimum' }],
+    'no-eval': 'off',
+    'no-multi-spaces': [
+      'error',
+      {
+        exceptions: {
+          VariableDeclarator: true,
+          ImportDeclaration: true
+        }
+      }
+    ],
+    'no-return-assign': 'off',
+    'prettier/prettier': 'error',
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'space-before-function-paren': ['error', 'never']
   }
-};
+}
